f82f32e9e2dd61d61df1478b3ed7957db759055a
SHAURYA AWASTHI <89693063+Shauryaa007@users.noreply.github.com>
2022-11-27 05:51:21 +0530

    27-11-2022

---
+++ Add Binary Strings.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+    string addBinary(string a, string b) {
+        int i = a.length()-1;
+        int j = b.length()-1;
+        string ans;
+        int carry = 0;
+        
+        while(i>=0 || j>=0 || carry){
+            if(i>=0){
+                carry += a[i] - '0';
+                i--;
+            }
+            if(j>=0){
+                carry += b[j] - '0';
+                j--;
+            }
+            
+            ans += (carry%2 + '0');
+            carry = carry/2;
+        }
+        
+        int jii=ans.length()-1;
+        int count=0;
+        while(jii>=0&&ans[jii]=='0')
+        {
+            if(ans[jii]=='0')
+            {
+              count++; 
+            }
+            jii--;
+            
+        }
+        ans=ans.substr(0,ans.length()-(count));
+        reverse(ans.begin(),ans.end());
+        return ans;
+    
+	}
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		string A, B; cin >> A >> B;
+		Solution ob;
+		cout << ob.addBinary (A, B);
+		cout << "\n";
+	}
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
